84f329901f76c4e215b5b32ade4b1db7f4686596
Parth <parthzarekar@gmail.com>
2023-11-09 21:53:54 +0530

    cors update

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from "cors";
 import LuxuriantRoutes from "./api/Luxuriant.routes.js";
-
-const app = express();
-
-app.use(cors({
-    origin:'*'
-}));
+const 
+import cors from "cors";
+const corsOrigin ={
+    origin:'http://localhost:5173', //or whatever port your frontend is using
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
 app.use(express.json());
 
 app.use("/api/v1/Luxuriant", LuxuriantRoutes);
