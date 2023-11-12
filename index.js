74ecd1500aa6ed3aa48b4432cea1afa54a60570b
Parth <parthzarekar@gmail.com>
2023-11-12 16:21:56 +0530

    code cleand up and commented

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+// Importing necessary modules and files
 import app from './server.js'
 import LuxuriantDAO from './dao/LuxuriantDAO.js'
 import mongodb from 'mongodb'
 
+// Creating a MongoClient object from mongodb
 const MongoClient = mongodb.MongoClient
+
+// Fetching MongoDB username and password from environment variables
 const mongo_username = process.env.mongo_username 
 const mongo_password = process.env.mongo_password
 
+// Constructing the MongoDB connection string
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.rjozjxo.mongodb.net/?retryWrites=true&w=majority`
 
-MongoClient.connect(uri).catch(err=>{
-    console.error(err.stack)
-    process.exit(1)
-}).catch(err=>{
-    console.error(err.stack)
-    process.exit(1)
-}).then(async client =>{
-    await LuxuriantDAO.InjectDB(client)
-    app.listen(process.env.PORT || 3000, ()=>{
-        console.log('Server is listening on port 3000')
+// Connecting to MongoDB
+MongoClient.connect(uri)
+    .catch(err => {
+        // If there's an error during connection, log the error and exit the process
+        console.error(err.stack)
+        process.exit(1)
     })
-})
+    .then(async client => {
+        // Once connected, inject the client into the LuxuriantDAO
+        await LuxuriantDAO.InjectDB(client)
+
+        // Start the server listening on the specified port
+        app.listen(process.env.PORT || 3000, () => {
+            console.log('Server is listening')
+        })
+    })
