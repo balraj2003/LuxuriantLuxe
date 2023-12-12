f3fcec40f63301e86f6385bbc570c11102232daa
Parth <parthzarekar@gmail.com>
2023-12-12 21:55:42 +0530

    send mail code changed 5 time

--- send_mail.js
+++ send_mail.js
@@ -3,57 +3,51 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export async function sendMail(customer, order, product) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.Mail_Usr,
-      pass: process.env.Mail_Pass,  
-    },
-  });
+	let transporter = nodemailer.createTransport({
+		host: "smtp-mail.outlook.com",
+		port: 587,
+		secure: false,
+		auth: {
+			user: process.env.Mail_Usr,
+			pass: process.env.Mail_Pass,
+		},
+	});
 
-  let orderDetails = "";
-  
-for (let item of order) {
-    let productItem = product.find(p =>{
-        return p._id.toString() === item.product_id.toString();
-    })
-    console.log("productItem: " + productItem)
-    let productName = productItem ? productItem.product_name : 'Product not found';
-    orderDetails += `Product name: ${productName}\nQuantity: ${item.quantity}\nPrice: ${item.price}\n\n`;
-}
+	let orderDetails = "";
 
-  let mailOptions = {
-    from: "balrajriotavanandi@outlook.com",
-    to: customer.customer_email,
-    subject: "Order Confirmation",
-    text: `Your order has been placed successfully. Here are your order details: \n${orderDetails}`,
-  };
-  const MAX_RETRIES = 3;
+	for (let item of order) {
+		let productItem = product.find((p) => {
+			return p._id.toString() === item.product_id.toString();
+		});
+		console.log("productItem: " + productItem);
+		let productName = productItem
+			? productItem.product_name
+			: "Product not found";
+		orderDetails += `Product name: ${productName}\nQuantity: ${item.quantity}\nPrice: ${item.price}\n\n`;
+	}
 
-  for (let i = 0; i < MAX_RETRIES; i++) {
-    try {
+	let mailOptions = {
+		from: process.env.Mail_Usr,
+		to: customer.customer_email,
+		subject: "Order Confirmation",
+		text: `Your order has been placed successfully. Here are your order details: \n${orderDetails}`,
+	};
 
-      let info = await new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(info);
-          }
-        });
-      });
-      console.log("Message sent: %s", info.messageId);
-      return true; // If the email was sent successfully, return true
-    } catch (error) {
-      console.error("Error occurred while sending email: %s", error);
-      if (i === MAX_RETRIES - 1) { // If this was the last attempt
-        console.error("Failed to send email after %s attempts", MAX_RETRIES);
-      } else {
-        console.log("Retrying to send email...");
-      }
-    }
-  }
-  return false; // If the email was not sent successfully, return false
+	let info = new Promise((resolve, reject) => {
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(info);
+			}
+		});
+	});
+
+	return info.then((data) => {
+    console.log("Mail sent successfully: " + data.response);
+    return true;
+	}).catch((error) => {
+    console.log("Error: " + error);
+    return false;
+	});
 }
