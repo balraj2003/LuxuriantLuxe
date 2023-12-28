5398d278699128e109ce024d7945ee9d916ce7b9
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-12-28 14:19:00 +0530

    removed password from product route

--- api/Luxuriant.controller.js
+++ api/Luxuriant.controller.js
@@ -1,5 +1,5 @@
 // Import necessary modules and initialize DAO
-import sendMail  from "../send_mail.js";
+import sendMail from "../send_mail.js";
 import LuxuriantDAO from "../dao/LuxuriantDAO.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,203 +8,210 @@ const master_password = process.env.Master_Password; // Get master password from
 
 // Define LuxuriantController class
 export default class LuxuriantController {
-  // Method to get Luxuriant data
-  static async apiGetLuxuriant(req, res, next) {
-    try {
-      // Send a JSON response with a message
-      res.json({ message: "Hello World!" });
-    } catch (e) {
-      // Send a 500 status code and the error message if an error occurs
-      res.status(500).json({ error: e.message });
-    }
-  }
+	// Method to get Luxuriant data
+	static async apiGetLuxuriant(req, res, next) {
+		try {
+			// Send a JSON response with a message
+			res.json({ message: "Hello World!" });
+		} catch (e) {
+			// Send a 500 status code and the error message if an error occurs
+			res.status(500).json({ error: e.message });
+		}
+	}
 
-  // Method to add orders
-  static async apiAddOrdersLuxuriant(req, res, next) {
-    try {
-      // Extract order details from the request query
-      const customer_email = req.query.customer_email;
-      const customer_phone = req.query.customer_phone;
-      const customer_address = req.query.customer_address;
-      const customer_name = req.query.customer_name;
-      const customer_order = req.query.customer_order;
-      const order_cost = req.query.order_cost;
+	// Method to add orders
+	static async apiAddOrdersLuxuriant(req, res, next) {
+		try {
+			// Extract order details from the request query
+			const customer_email = req.query.customer_email;
+			const customer_phone = req.query.customer_phone;
+			const customer_address = req.query.customer_address;
+			const customer_name = req.query.customer_name;
+			const customer_order = req.query.customer_order;
+			const order_cost = req.query.order_cost;
 
-      // Add the order using the DAO
-      const order = await dao.addOrder(
-        customer_email,
-        customer_phone,
-        customer_address,
-        customer_name,
-        customer_order,
-        order_cost
-      );
+			// Add the order using the DAO
+			const order = await dao.addOrder(
+				customer_email,
+				customer_phone,
+				customer_address,
+				customer_name,
+				customer_order,
+				order_cost
+			);
 
-      // Send a JSON response with the order details if the order was added successfully
-      if (order.message === "Success") {
-        res.json({
-          order_id: order.order_id,
-          order_cost: order.order_cost,
-          payment_status: order.payment_status,
-          message: order.message,
-        });
-      } else {
-        // Send a JSON response with the error message if the order was not added successfully
-        res.json({ message: order.message });
-      }
-    } catch (e) {
-      // Send a 500 status code and the error message if an error occurs
-      res.status(500).json({ error: e.message });
-    }
-  }
+			// Send a JSON response with the order details if the order was added successfully
+			if (order.message === "Success") {
+				res.json({
+					order_id: order.order_id,
+					order_cost: order.order_cost,
+					payment_status: order.payment_status,
+					message: order.message,
+				});
+			} else {
+				// Send a JSON response with the error message if the order was not added successfully
+				res.json({ message: order.message });
+			}
+		} catch (e) {
+			// Send a 500 status code and the error message if an error occurs
+			res.status(500).json({ error: e.message });
+		}
+	}
 
-  // Method to get customers
-  static async apiGetCustomers(req, res, next) {
-    try {
-      // Check if the provided password matches the master password
-      const pass = req.body.password;
-      if (pass === master_password) {
-        // Get the customers using the DAO
-        const customers = await dao.getCustomers();
+	// Method to get customers
+	static async apiGetCustomers(req, res, next) {
+		try {
+			// Check if the provided password matches the master password
+			const pass = req.body.password;
+			if (pass === master_password) {
+				// Get the customers using the DAO
+				const customers = await dao.getCustomers();
 
-        // Send a JSON response with the customers if they were found
-        if (customers) {
-          res.json({customers: customers, message: "Success"});
-        } else {
-          // Send a JSON response with an error message if no customers were found
-          res.json({ message: "No customers found" });
-        }
-      } else {
-        // Send a JSON response with an error message if the password is incorrect
-        res.json({ message: "Incorrect password" });
-      }
-    } catch (e) {
-      // Send a 500 status code and the error message if an error occurs
-      res.status(500).json({ error: e.message });
-    }
-  }
+				// Send a JSON response with the customers if they were found
+				if (customers) {
+					res.json({ customers: customers, message: "Success" });
+				} else {
+					// Send a JSON response with an error message if no customers were found
+					res.json({ message: "No customers found" });
+				}
+			} else {
+				// Send a JSON response with an error message if the password is incorrect
+				res.json({ message: "Incorrect password" });
+			}
+		} catch (e) {
+			// Send a 500 status code and the error message if an error occurs
+			res.status(500).json({ error: e.message });
+		}
+	}
 
-  // Method to get products
-  static async apiGetProducts(req, res, next) {
-    try {
-      // Check if the provided password matches the master password
-      const pass = req.body.password;
-      if (pass === master_password) {
-        // Get the products using the DAO
-        const products = await dao.getProducts();
+	// Method to get products
+	static async apiGetProducts(req, res, next) {
+		try {
+			// Check if the provided password matches the master password
+			// const pass = req.body.password;
+			// if (pass === master_password) {
+			// Get the products using the DAO
+			const products = await dao.getProducts();
 
-        // Send a JSON response with the products if they were found
-        if (products) {
-          res.json({ products: products, message: "Success"});
-        } else {
-          // Send a JSON response with an error message if no products were found
-          res.json({ message: "No products found" });
-        }
-      } else {
-        // Send a JSON response with an error message if the password is incorrect
-        res.json({ message: "Incorrect password" });
-      }
-    } catch (e) {
-      // Send a 500 status code and the error message if an error occurs
-      res.status(500).json({ error: e.message });
-    }
-  }
+			// Send a JSON response with the products if they were found
+			if (products) {
+				res.json({ products: products, message: "Success" });
+			} else {
+				// Send a JSON response with an error message if no products were found
+				res.json({ message: "No products found" });
+			}
+			// } else {
+			//   // Send a JSON response with an error message if the password is incorrect
+			//   res.json({ message: "Incorrect password" });
+			// }
+		} catch (e) {
+			// Send a 500 status code and the error message if an error occurs
+			res.status(500).json({ error: e.message });
+		}
+	}
 
-  // Method to get orders
-  static async apiGetOrders(req, res, next) {
-    try {
-      // Check if the provided password matches the master password
-      const pass = req.body.password;
-      if (pass === master_password) {
-        // Get the orders using the DAO
-        const orders = await dao.getOrders();
+	// Method to get orders
+	static async apiGetOrders(req, res, next) {
+		try {
+			// Check if the provided password matches the master password
+			const pass = req.body.password;
+			if (pass === master_password) {
+				// Get the orders using the DAO
+				const orders = await dao.getOrders();
 
-        // Send a JSON response with the orders if they were found
-        if (orders) {
-          res.json({ orders: orders, message: "Success" });
-        } else {
-          // Send a JSON response with an error message if no orders were found
-          res.json({ message: "No orders found" });
-        }
-      } else {
-        // Send a JSON response with an error message if the password is incorrect
-        res.json({ message: "Incorrect password" });
-      }
-    } catch (e) {
-      // Send a 500 status code and the error message if an error occurs
-      res.status(500).json({ error: e.message });
-    }
-  }
+				// Send a JSON response with the orders if they were found
+				if (orders) {
+					res.json({ orders: orders, message: "Success" });
+				} else {
+					// Send a JSON response with an error message if no orders were found
+					res.json({ message: "No orders found" });
+				}
+			} else {
+				// Send a JSON response with an error message if the password is incorrect
+				res.json({ message: "Incorrect password" });
+			}
+		} catch (e) {
+			// Send a 500 status code and the error message if an error occurs
+			res.status(500).json({ error: e.message });
+		}
+	}
 
-  // Method to change payment status
-  static async apiChangePaymentStatus(req, res, next) {
-      try {
-        const pass = req.body.password;
-        const order_id = req.body.order_id;
-        const payment_status = req.body.payment_status;
+	// Method to change payment status
+	static async apiChangePaymentStatus(req, res, next) {
+		try {
+			const pass = req.body.password;
+			const order_id = req.body.order_id;
+			const payment_status = req.body.payment_status;
 
-        if (pass === master_password) {
-          const order = await dao.changePaymentStatus(order_id, payment_status);
-          const customer = await dao.getCustomerEmail(order.customer_id);
-          const product = await dao.getProducts();
+			if (pass === master_password) {
+				const order = await dao.changePaymentStatus(
+					order_id,
+					payment_status
+				);
+				const customer = await dao.getCustomerEmail(order.customer_id);
+				const product = await dao.getProducts();
 
-          if (order) {
-           const mail = await sendMail(customer,order.order_details,product);
-           if (mail){
-             res.json({order:order, message: "Success" });
-           }else{
-            res.json({message: "Failure"})
-           }
-          } else {
-            res.json({ message: "No orders found" });
-          }
-        } else {
-          res.status(401).json({ message: "Unauthorized" });
-        }
-      } catch (e) {
-        console.error(e);
-        res.status(500).json({ message: "An error occurred" });
-      }
-    }
-  // Method to check password
-  static async apiCheckPassword(req, res) {
-    try {
-      // Check if the provided password matches the master password
-      const pass = req.body.password;
-      if (pass === master_password) {
-        // Send a JSON response with a success message if the password is correct
-        res.json({ message: "Success" });
-      } else {
-        // Send a JSON response with a failure message if the password is incorrect
-        res.json({ message: "Failure" });
-      }
-    } catch (e) {
-      // Send a 500 status code and the error message if an error occurs
-      res.status(500).json({ error: e.message });
-    }
-  }
+				if (order) {
+					const mail = await sendMail(
+						customer,
+						order.order_details,
+						product
+					);
+					if (mail) {
+						res.json({ order: order, message: "Success" });
+					} else {
+						res.json({ message: "Failure" });
+					}
+				} else {
+					res.json({ message: "No orders found" });
+				}
+			} else {
+				res.status(401).json({ message: "Unauthorized" });
+			}
+		} catch (e) {
+			console.error(e);
+			res.status(500).json({ message: "An error occurred" });
+		}
+	}
+	// Method to check password
+	static async apiCheckPassword(req, res) {
+		try {
+			// Check if the provided password matches the master password
+			const pass = req.body.password;
+			if (pass === master_password) {
+				// Send a JSON response with a success message if the password is correct
+				res.json({ message: "Success" });
+			} else {
+				// Send a JSON response with a failure message if the password is incorrect
+				res.json({ message: "Failure" });
+			}
+		} catch (e) {
+			// Send a 500 status code and the error message if an error occurs
+			res.status(500).json({ error: e.message });
+		}
+	}
 
-  // Method to add products
-  static async apiAddProductsLuxuriant(req, res, next) {
-    try {
-      // Add a product using the DAO
-      const product = await dao.addProduct();
+	// Method to add products
+	static async apiAddProductsLuxuriant(req, res, next) {
+		try {
+			// Add a product using the DAO
+			const product = await dao.addProduct();
 
-      // Send a JSON response with the product details if the product was added successfully
-      if (product) {
-        res.json({
-          product_id: product.product_id,
-          product_name: product.product_name,
-          product_cost: product.product_cost,
-          message: "Product added successfully",
-        });
-      } else {
-        // Send a JSON response with an error message if the product was not added successfully
-        res.json({ message: "Failure in adding product" });
-      }
-    } catch (e) {
-      // Send a 500 status code and the error message if an error occurs
-      res.status(500).json({ error: e.message });
-    }
-  }
+			// Send a JSON response with the product details if the product was added successfully
+			if (product) {
+				res.json({
+					product_id: product.product_id,
+					product_name: product.product_name,
+					product_cost: product.product_cost,
+					message: "Product added successfully",
+				});
+			} else {
+				// Send a JSON response with an error message if the product was not added successfully
+				res.json({ message: "Failure in adding product" });
+			}
+		} catch (e) {
+			// Send a 500 status code and the error message if an error occurs
+			res.status(500).json({ error: e.message });
+		}
+	}
 }
